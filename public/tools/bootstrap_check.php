2354aa678e067365518a4896396e057fb8f78d7d
Zanitakos <omar2886@gmail.com>
2025-12-16 13:09:21 +0100

    Add tools guard and sanitize configuration

--- public/tools/bootstrap_check.php
+++ public/tools/bootstrap_check.php
@@ -1,5 +1,6 @@
 <?php
 declare(strict_types=1);
+require_once __DIR__ . '/_guard.php';
 
 header('Content-Type: application/json');
 
--- public/tools/run_fuzz.php
+++ public/tools/run_fuzz.php
@@ -1,5 +1,6 @@
 <?php
 declare(strict_types=1);
+require_once __DIR__ . '/_guard.php';
 
 // CONFIG: token compartido (mejor por env si lo tenéis)
 const FUZZ_WEB_TOKEN = 'REEMPLAZAR_CON_SECRETO_UNICO';
